b0879db9ed55efc2e54679437d76744071edcc79
Pranjali Garg <pg0224@srmist.edu.in>
2026-05-08 18:47:50 +0530

    Restore original Django settings in settings.py

--- payroll_manager/settings.py
+++ payroll_manager/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'payroll_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'payroll_manager', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
