c215a125093d90b07ec53ae833b6e2685f62eeb8
Pranjali Garg <pg0224@srmist.edu.in>
2026-05-08 20:04:41 +0530

    Update settings.py

--- payroll_manager/settings.py
+++ payroll_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f2zx8*lb*em*-*b+!&1lpp&$_9q9kmkar+l3x90do@s(+sr&x7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
